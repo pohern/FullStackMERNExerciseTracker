8f1a1c43e6f149fb1941d0dd66332ec9ef6de6b8
pohern <ohern_p@yahoo.com>
2021-02-16 16:35:56 -0600

    update styling issue

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import NavBar from "./components/navbar";
-import ExerciseList from "./components/exercises-list";
-import EditExercise from "./comonents/edit-exerecise";
+import ExercisesList from "./components/exercises-list";
+import EditExercise from "./components/edit-exercise";
 import CreateExercise from "./components/create-exercise";
 import CreateUser from "./components/create-user";
 
 function App() {
   return (
     <Router>
-      <NavBar />
-      <br />
-      <Route path='/' exact component={ExerciseList} />
-      <Route path='/edit/:id' component={EditExercise} />
-      <Route path='/create' component={CreateExercise} />
-      <Route path='/user' component={CreateUser} />
+      <div className='container'>
+        <NavBar />
+        <br />
+        <Route path='/' exact component={ExercisesList} />
+        <Route path='/edit/:id' component={EditExercise} />
+        <Route path='/create' component={CreateExercise} />
+        <Route path='/user' component={CreateUser} />
+      </div>
     </Router>
   );
 }
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -8,7 +8,7 @@ export default class Navbar extends Component {
         <Link to='/' className='navbar-brand'>
           ExcerTracker
         </Link>
-        <div className='collpase navbar-collapse'>
+        <div className='collapse navbar-collapse'>
           <ul className='navbar-nav mr-auto'>
             <li className='navbar-item'>
               <Link to='/' className='nav-link'>
