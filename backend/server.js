b5eb53db3466cdd39d44f293daf7bf7e52c5fb08
pohern <ohern_p@yahoo.com>
2021-02-16 14:39:31 -0600

    add routes for both user and exercise

--- backend/server.js
+++ backend/server.js
@@ -24,6 +24,12 @@ db.once("open", () => {
   console.log("MongoDB database connection established successfully");
 });
 
+const exercisesRouter = require('./routes/exercises')
+const usersRouter = require('./routes/users')
+
+app.use('/exercises', exercisesRouter)
+app.use('/users', usersRouter)
+
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
 });
