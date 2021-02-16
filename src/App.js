6dbbbcf510b7c4daf9c1267fe3a08cb918dcda3d
pohern <ohern_p@yahoo.com>
2021-02-16 15:52:19 -0600

    add navar to components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import NavBar from "./components/navbar";
+import ExerciseList from "./components/exercises-list";
+import EditExercise from "./comonents/edit-exerecise";
+import CreateExercise from "./components/create-exercise";
+import CreateUser from "./components/create-user";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <NavBar />
+      <br />
+      <Route path='/' exact component={ExerciseList} />
+      <Route path='/edit/:id' component={EditExercise} />
+      <Route path='/create' component={CreateExercise} />
+      <Route path='/user' component={CreateUser} />
+    </Router>
   );
 }
 
