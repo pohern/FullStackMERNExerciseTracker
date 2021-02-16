09672143fa7f9e991b1feb3b27ddee0f25240051
pohern <ohern_p@yahoo.com>
2021-02-16 14:08:38 -0600

    add .env and connect to database

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.DATABASE_URL;
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+});
+
+const db = mongoose.connection;
+
+db.on("connected", () => {
+  console.log("MongoDb database connection established successfully");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
